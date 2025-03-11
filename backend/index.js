d81fd88453c2866ffa24965d32f7c1a899186a74
Alexandros Polymeros <alpolymer@gmail.com>
2025-03-11 12:29:40 +0200

    removed cors

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,6 @@
 const express = require("express")
 const app = express()
 const morgan = require("morgan")
-const cors = require("cors")
 
 app.use(express.static("dist"))
 app.use(express.json())
@@ -13,8 +12,6 @@ morgan.token("body", (req) => {
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 )
-
-app.use(cors())
 
 let entries = [
   {
